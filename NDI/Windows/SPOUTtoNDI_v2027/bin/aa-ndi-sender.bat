395454687485b9c2356abcd9a6158f6d155ef27e
Vojtěch Leischner <50262885+trackme518@users.noreply.github.com>
2025-11-14 15:40:45 +0100

    added Spout and NDI utilities

---
+++ NDI/Windows/SPOUTtoNDI_v2027/bin/aa-ndi-sender.bat
@@ -0,0 +1,15 @@
+@echo off
+rem
+rem Command line for "NDI to Spout.exe"
+rem
+rem The NDI name is composed of two parts
+rem The computer name and the sender name
+rem The computer name is unique for the particular computer
+rem in this case "DESKTOP-P706Q09". Change it as required.
+rem Note that "Studio Monitor" precedes the name with a prefix "NDI - "
+rem e.g. "NDI - DESKTOP-P706Q09 (Test Pattern)"
+rem Do not include this prefix.
+rem
+@echo on
+"NDI to Spout.exe" "DESKTOP-P706Q09 (Test Pattern)"
+
---
+++ NDI/Windows/SPOUTtoNDI_v2027/bin/aa-spout-sender.bat
@@ -0,0 +1,7 @@
+@echo off
+rem
+rem Command line for "Spout to NDI.exe"
+rem
+@echo on
+"Spout to NDI.exe" "Spout Sender"
+
